d300fb5e0339851cf511a2ef0d25791f7d5fbf77
MOHAMED M S NASSER <mms.nasser@qu.edu.qa>
2019-09-14 02:53:53 +0300

    Add files via upload

--- pcm/evalud.m
+++ pcm/evalud.m
@@ -1,17 +1,17 @@
-function w = evalu (f,z,b)
-% evalu.m 
+function w = evalud (f,z,b)
+% evalud.m 
 % Nasser, September 6, 2019
 % 
 % Let f be the conformal mapping from the polygonal domain G onto the
 % circular domain D and g be its invers.
-% For b='d', this function computes w=f(z) where z is a vector of points
+% For b='d', this function computes w=f'(z) where z is a vector of points
 % in G 
-% For b='v', this function computes w=g(z) where z is a vector of points
+% For b='v', this function computes w=g'(z) where z is a vector of points
 % in D 
 % 
 % 
 % Input:
-%    1) f, an object with:
+%  1) f, an object with:
 % 
 % f.alpha = alpha (alpha is an auxiliary point in the domain G if G is
 %                  bounded and alpha=inf if G is unbounded) 
@@ -73,15 +73,14 @@ if b=='d'
     %
     zz  = z(:).';
     if abs(alpha)<inf
-        w4  = fcaun(et,etp,zet,zz,nv);
+        w4  = fcaunp(et,etp,zetp,zz,nv);
     elseif abs(alpha)==inf
         if isempty(f.inf)
             c2 = 1;
         else
             c2  = f.inf;
         end
-        cnto(1) = getInteriorPoint(et(1:nv(1)), etp(1:nv(1)));
-        w4  = (zz-cnto(1)).*fcaun(et,etp,zet./(et-cnto(1)),zz,nv,c2);
+        w4  = fcaunp(et,etp,zetp,zz,nv,c2);
     end
     if isrow(z)==1
         w=w4(:).';
@@ -93,95 +92,14 @@ elseif b=='v'
     %
     w4  = z(:).';
     if abs(alpha)<inf
-        zz  = fcaun(zet,zetp,et,w4,nv);
+        zz  = fcaunp(zet,zetp,etp,w4,nv);
     elseif abs(alpha)==inf
         if isempty(f.inf)
             c2v = 1;
         else
             c2v  = 1./f.inf;
         end
-        zz  = (w4-cent(1)).*fcaun(zet,zetp,et./(zet-cent(1)),w4,nv,c2v);
-    end
-    if isrow(z)==1
-        w=zz(:).';
-    else
-        w=zz(:);
-    end
-    %
-elseif b=='db'
-    %
-    zz   = z(:).';
-    ind = 0;
-    zb  = [];
-    for k4=1:length(zz)
-        [minj,j]=min(abs(zz(k4)-et));
-        if minj>=1e-6
-            ind = ind+1;
-            zb(ind)=zz(k4);
-        end
-    end
-    if length(zb)>0
-        if abs(alpha)<inf
-            wb  = fcaun(et,etp,zet,zb,nv);
-        elseif abs(alpha)==inf
-            if isempty(f.inf)
-                c2 = 1;
-            else
-                c2  = f.inf;
-            end
-            cnto(1) = getInteriorPoint(et(1:nv(1)), etp(1:nv(1)));
-            wb  = (zb-cnto(1)).*fcaun(et,etp,zet./(et-cnto(1)),zb,nv,c2);
-        end
-    end
-    ind = 0;
-    for k4=1:length(zz)
-        [minj,j]=min(abs(zz(k4)-et));
-        if minj<1e-6
-            w4(k4,1)=zet(j);
-        else
-            ind = ind+1;
-            w4(k4,1)=wb(ind);
-        end
-    end
-    if isrow(z)==1
-        w=w4(:).';
-    else
-        w=w4(:);
-    end
-    %
-elseif b=='vb'
-    %
-    w4  = z(:).';
-    ind = 0;
-    wb  = [];
-    for k4=1:length(w4)
-        [minj,j]=min(abs(w4(k4)-zet));
-        if minj>=1e-6
-            ind = ind+1;
-            wb(ind)=w4(k4);
-        end
-    end
-    if length(wb)>0
-        if abs(alpha)<inf
-            zb  = fcaun(zet,zetp,et,wb,nv);
-        elseif abs(alpha)==inf
-            if isempty(f.inf)
-                c2v = 1;
-            else
-                c2v  = 1./f.inf;
-            end
-            zb  = (wb-cent(1)).*fcaun(zet,zetp,et./(zet-cent(1)),wb,nv,c2v);
-        end
-    end
-    ind = 0;
-    for k4=1:length(w4)
-        [minj,j]=min(abs(w4(k4)-zet));
-        if minj<1e-6
-            zz(k4,1)=et(j);
-        else
-            ind = ind+1;
-            zz(k4,1)=zb(ind);
-        end
+        zz  = fcaunp(zet,zetp,etp,w4,nv,c2v);
     end
     if isrow(z)==1
         w=zz(:).';
